75c3eb9d7345b7507e6129a5c58598bfc218335d
eaU520 <ecallowayllc@gmail.com>
2023-07-06 00:49:02 -0400

    First component

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Price from 'components/Price';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>First Page</Text>
       <StatusBar style="auto" />
+      {/* <Price /> */}
     </View>
   );
 }
